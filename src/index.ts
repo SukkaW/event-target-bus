43408c9809e4d75feab62295d8d7fe8ca73bf6e3
SukkaW <isukkaw@gmail.com>
2026-02-28 22:37:09 +0800

    feat: support more event target in types

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-export type SupportedEventTarget = Window | Document | HTMLElement | SVGElement | Element | VisualViewport | ShadowRoot | XMLHttpRequest | Performance | Worker | CookieStore | MediaQueryList | EventTarget;
+export type SupportedEventTarget = Window | Document | HTMLElement | SVGElement | Element | VisualViewport | ShadowRoot | XMLHttpRequest | XMLHttpRequestEventTarget | Performance | Worker | CookieStore | MediaQueryList | PermissionStatus | ScreenOrientation | AbortSignal | EventTarget;
 
-export type EventMapFor<T extends SupportedEventTarget> = T extends Window ? WindowEventMap & {
-  // TS 4.9.5 does not support `freeze` and `resume` events yet
-  freeze: Event,
-  resume: Event,
-  // TS 4.9.5 does not define `visibilitychange` on Window (only Document)
-  visibilitychange: Event
-}
+/* eslint-disable @stylistic/indent -- flat types is more readable */
+export type EventMapFor<T extends SupportedEventTarget> = |
+  T extends Window ? WindowEventMap & {
+    // TS 4.9.5 does not support `freeze` and `resume` events yet
+    freeze: Event,
+    resume: Event,
+    // TS 4.9.5 does not define `visibilitychange` on Window (only Document)
+    visibilitychange: Event
+  }
   : T extends Document ? DocumentEventMap
-    : T extends HTMLMediaElement ? HTMLMediaElementEventMap
-      : T extends HTMLElement ? HTMLElementEventMap
-        : T extends SVGElement ? SVGElementEventMap
-          : T extends Element ? ElementEventMap
-            : T extends VisualViewport ? VisualViewportEventMap
-              /**
-               * ShadowRootEventMap is not yet defined in our supported TS version. Instead, use
-               * GlobalEventHandlersEventMap which is more than enough as we only need to listen for events bubbling
-               * through the ShadowRoot like "change" or "input"
-               */
-              : T extends ShadowRoot ? GlobalEventHandlersEventMap
-                : T extends XMLHttpRequest ? XMLHttpRequestEventMap
-                  : T extends Performance ? PerformanceEventMap
-                    : T extends Worker ? WorkerEventMap
-                      : T extends CookieStore ? CookieStoreEventMap
-                        : T extends MediaQueryList ? MediaQueryListEventMap
-                          : T extends PermissionStatus ? PermissionStatusEventMap
-                            : T extends ScreenOrientation ? ScreenOrientationEventMap
-                              : T extends EventTarget ? Record<string, Event>
-                                : Record<never, never>;
+  : T extends HTMLMediaElement ? HTMLMediaElementEventMap
+  : T extends HTMLElement ? HTMLElementEventMap
+  : T extends SVGElement ? SVGElementEventMap
+  : T extends Element ? ElementEventMap
+  : T extends VisualViewport ? VisualViewportEventMap
+  /**
+   * ShadowRootEventMap is not yet defined in our supported TS version. Instead, use
+   * GlobalEventHandlersEventMap which is more than enough as we only need to listen for events bubbling
+   * through the ShadowRoot like "change" or "input"
+   */
+  : T extends ShadowRoot ? GlobalEventHandlersEventMap & ShadowRootEventMap
+  : T extends XMLHttpRequest ? XMLHttpRequestEventMap
+  : T extends XMLHttpRequestEventTarget ? XMLHttpRequestEventTargetEventMap
+  : T extends Performance ? PerformanceEventMap
+  : T extends Worker ? WorkerEventMap
+  : T extends CookieStore ? CookieStoreEventMap
+  : T extends MediaQueryList ? MediaQueryListEventMap
+  : T extends PermissionStatus ? PermissionStatusEventMap
+  : T extends ScreenOrientation ? ScreenOrientationEventMap
+  : T extends AbortSignal ? AbortSignalEventMap
+  : T extends EventTarget ? Record<string, Event>
+  : Record<never, never>;
+/* eslint-enable @stylistic/indent */
 
 export type EventTargetListener<E = Event> = (event: E) => void;
 
